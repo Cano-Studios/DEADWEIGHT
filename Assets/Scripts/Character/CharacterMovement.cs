045531b7d68c42a645d55d71d495e5e34290ad65
carsonSgit <92652800+carsonSgit@users.noreply.github.com>
2024-07-20 10:20:34 -0400

    Improved Coyote jump
    Followed tutorial: https://www.youtube.com/watch?v=RFix_Kg2Di0&t=153s

--- Assets/Scripts/Character/CharacterMovement.cs
+++ Assets/Scripts/Character/CharacterMovement.cs
@@ -4,19 +4,43 @@ public class CharacterMovement : MonoBehaviour
 {
     public Vector2 speed = new Vector2(50, 100);
     public Rigidbody2D rb;
-    public bool isJumping;
+    public bool isJumping = false;
+    private float coyoteTime = 0.2f;
+    private float coyoteTimeCounter;
+    private float jumpBufferTime = 0.2f;
+    private float jumpBufferCounter;
 
+
+    // Coyote jump based off @see:https://www.youtube.com/watch?v=RFix_Kg2Di0&t=153s
     void Start()
     {
-        rb.velocity = new Vector2(0, 0);
+        rb.velocity = Vector2.zero;
+        coyoteTimeCounter = coyoteTime;
+        jumpBufferCounter = 0f;
     }
+
     void Update()
     {
         float inputX = Input.GetAxis("Horizontal");
         rb.velocity = new Vector2(speed.x * inputX, rb.velocity.y);
-        if (!isJumping && Input.GetButtonDown("Jump"))
+
+        // Update coyote timer 
+        if (isJumping)
+            coyoteTimeCounter -= Time.deltaTime;
+        else
+            coyoteTimeCounter = coyoteTime;
+
+        // Update jump buffer timerr
+        if (Input.GetButtonDown("Jump"))
+            jumpBufferCounter = jumpBufferTime;
+        else
+            jumpBufferCounter -= Time.deltaTime;
+
+        if (jumpBufferCounter > 0f && (coyoteTimeCounter > 0f || !isJumping))
         {
             rb.velocity = new Vector2(rb.velocity.x, speed.y);
+            isJumping = true;
+            jumpBufferCounter = 0f;
         }
     }
 
@@ -28,7 +52,6 @@ public class CharacterMovement : MonoBehaviour
         }else if (other.gameObject.CompareTag("Chain"))
         {
             Physics2D.IgnoreCollision(other.collider, GetComponent<Collider2D>());
-        
         }
     }
 
@@ -44,14 +67,7 @@ public class CharacterMovement : MonoBehaviour
     {
         if (other.gameObject.CompareTag("Ground"))
         {
-            // coyote jump
-            Invoke(nameof(SetJumping), 0.5f);
+            isJumping = true;
         }
     }
-
-    void SetJumping()
-    {
-        isJumping = true;
-    }
-
 }
